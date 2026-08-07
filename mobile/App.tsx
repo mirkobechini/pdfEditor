fbdb7273c8e496df317717ce9b82f11dc0a134ac
mirkobechini <mirkobechini@gmail.com>
2026-08-07 18:31:06 +0200

    feat(mobile): apply darkTheme based on system color scheme (issue-617)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PaperProvider } from "react-native-paper";
@@ -6,13 +7,16 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "./src/shared/auth";
 import AppNavigator from "./src/navigation/AppNavigator";
 import * as Icons from "@expo/vector-icons";
-import { lightTheme } from "./src/theme";
+import { lightTheme, darkTheme } from "./src/theme";
 
 export default function App() {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === "dark" ? darkTheme : lightTheme;
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <PaperProvider
-        theme={lightTheme}
+        theme={theme}
         settings={{
           icon: (props) => <Icons.MaterialCommunityIcons {...props} />,
         }}
